87e6cd09990b0661ef64a713a78208cefaeef639
Nikita Ramnjonok <92184855+NikitaRamnionak@users.noreply.github.com>
2021-12-11 21:57:31 +0300

    Add class fields

--- src/bsu/rfe/java/group7/lab2/Ramnenok/varС1/Main_Frame.java
+++ src/bsu/rfe/java/group7/lab2/Ramnenok/varС1/Main_Frame.java
@@ -1,4 +1,32 @@
 package bsu.rfe.java.group7.lab2.Ramnenok.varС1;
 
-public class Main_Frame {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.image.ImageObserver;
+import java.lang.StrictMath;
+import java.text.AttributedCharacterIterator;
+
+import static java.lang.Math.*;
+
+public class Main_Frame extends JFrame {
+    private static final int width = 1300;
+    private static final int height = 500;
+    private JTextField result_field;
+    private JTextField x_field;
+    private JTextField y_field;
+    private JTextField z_field;
+    private JLabel image;
+    private ButtonGroup radioButtons = new ButtonGroup();
+    private Box formula_type = Box.createHorizontalBox();
+    private ButtonGroup radioMemoryButtons = new ButtonGroup();
+    private Box hBoxMemoryType = Box.createHorizontalBox();
+    private JTextField memoryTextField;
+    int formula_number = 1;
+    private int memoryId = 1;
+
+    private Double mem1 = 0.0;
+    private Double mem2 = 0.0;
+    private Double mem3 = 0.0;
 }
