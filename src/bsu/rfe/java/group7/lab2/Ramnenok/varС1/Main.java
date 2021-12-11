60c341e972e8dc1b6ab0d37872138dfe48c501cc
Nikita Ramnjonok <92184855+NikitaRamnionak@users.noreply.github.com>
2021-12-11 23:20:27 +0300

    Main update

--- src/bsu/rfe/java/group7/lab2/Ramnenok/varС1/Main.java
+++ src/bsu/rfe/java/group7/lab2/Ramnenok/varС1/Main.java
@@ -1,8 +1,13 @@
 package bsu.rfe.java.group7.lab2.Ramnenok.varС1;
 
+import javax.swing.JFrame;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        Main_Frame frame = new Main_Frame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
